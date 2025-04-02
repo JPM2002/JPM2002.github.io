8e47d95ce059b87fe00c3a4e882dd33bc99639c5
Javier Pozo Miranda <76562009+JPM2002@users.noreply.github.com>
2025-04-02 16:19:38 -0400

    back to zero

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'export',
-    basePath: '/JPM2002.github.io',
-    assetPrefix: '/JPM2002.github.io/',
-  };
-  
-  module.exports = nextConfig;
-  
